617de2ebfae41b6274db323f30b7df593ef82ef3
Samuel Butler <sambutler1017@icloud.com>
2022-08-28 14:46:31 -0400

    no message

--- src/main/java/com/hydro/HydroTestApplication.java
+++ src/main/java/com/hydro/HydroTestApplication.java
@@ -3,12 +3,12 @@ package com.hydro;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * Empty main for building out jar file.
+ * Empty main for building out jar file for test microservice.
  * 
  * @author Sam Butler
  * @since August 27, 2022
  */
-public class HydroCommonApplication {
+public class HydroTestApplication {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {}
 }
